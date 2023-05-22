f049b7e27fe1b10ee9eb4c42e72ca781d6df68a2
Eunggseo <103013141+Eunggseo@users.noreply.github.com>
2023-05-21 21:21:34 -0700

    explore test 1.1

--- sw.js
+++ sw.js
@@ -3,36 +3,42 @@
 
 const CACHE_NAME = 'lab-7-starter';
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      // B6. Add all of the URLs from RECIPE_URLs to the cache
-      return cache.addAll(RECIPE_URLs);
-    })
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
     // B7. Open the cache using the name we gave above (CACHE_NAME)
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.match(event.request).then(function(response) {
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
         if (response) {
           // B8. If the request is in the cache, return the cached version
           return response;
         } else {
           // Fetch the resource, add it to the cache, and return network response
-          return fetch(event.request).then(function(networkResponse) {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          });
+          return fetch(event.request)
+            .then(function (networkResponse) {
+              // Check if we received a valid response
+              if (
+                !networkResponse ||
+                networkResponse.status !== 200 ||
+                networkResponse.type !== 'basic'
+              ) {
+                return networkResponse;
+              }
+
+              // Clone the response since it can only be consumed once
+              const responseToCache = networkResponse.clone();
+
+              // Add the response to the cache
+              caches.open(CACHE_NAME).then(function (cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+              return networkResponse;
+            })
+            .catch(function (error) {
+              console.error('Error fetching resource:', error);
+            });
         }
       });
     })
   );
 });
-
